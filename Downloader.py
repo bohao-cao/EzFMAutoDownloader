44fba4e234c73fb6b4df6bfc652fa426d24396a9
bohaocao <bohao.cao@gmail.com>
2014-10-09 22:19:49 -0400

    Add logging, delete files older than 7 days. Recognize OS.

--- Downloader.py
+++ Downloader.py
@@ -5,21 +5,37 @@ import time
 import sys
 import os
 import shutil
+from os.path import isfile, join
+import logging
+import datetime
 #http://mod.cri.cn/eng/ez/morning/2014/ezm140721.mp3
 
-def myprint():
-    print('hello')
+logging.basicConfig(filename='log.log',format='%(asctime)s %(message)s', datefmt='%m/%d/%y %A')
 
 ezFmBaseUrl = 'http://mod.cri.cn/eng/ez/morning/2014/ezm'
 mp3Suffix = '.mp3'
 
-persistPath = 'D:\\EzFM\\'
+if sys.platform.startswith('win'):
+    persistPath = 'D:\\EzFM\\'
+elif sys.platform.startswith('darwin'):
+    persistPath = '/Users/bohaocao/Dropbox/EzFm/'
+
 if not os.path.exists(persistPath):
     os.mkdir(persistPath)
 
-
 todayDateTime = time.strftime("%y%m%d")
+todayWeekday = time.strftime('%A')
+if todayWeekday.startswith('Sun') or todayWeekday.startswith('Sat'):
+    logging.info('Today is weekend, program will not run')
+    exit()
 
+#find files that are older than 7 days.
+files = [f for f in os.listdir(persistPath) if isfile(join(persistPath, f))]
+
+for f in files:
+    if time.time() - os.path.getctime(join(persistPath,f)) > 7*60*60*24:
+        logging.info('Remove old file' + persistPath + f)
+        os.remove(join(persistPath,f))
 
 dateTimeToUse = todayDateTime
 
@@ -29,19 +45,18 @@ fileN = dateTimeToUse + mp3Suffix
 
 try:
     response = request.urlopen(url)
-    myprint()
+    logging.info('Downloading' + fileN + 'starts.')
 except error.HTTPError as err:
-    print('Failed to open mp3 file at date:' + dateTimeToUse + ' .HTTP error code: ' + str(err.code))
-
+    logging.error('Failed to open mp3 file at date:' + dateTimeToUse + ' .HTTP error code: ' + str(err.code))
     exit()
 except Exception as inst:
-    print(type(inst))
+    logging.error(type(inst))
     exit()
 
 if not os.path.exists(os.path.join(persistPath + fileN)):
     with open(os.path.join(persistPath + fileN), 'wb') as out_file:
         shutil.copyfileobj(response, out_file)
 
-
-
+logging.info('File download successful to ' + persistPath + fileN)
+logging.info('Exit')
 
